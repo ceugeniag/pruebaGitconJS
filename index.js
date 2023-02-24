771c495fe09915b81ca09c388ced8725e13a30de
Eugenia Gonzalez <115852517+ceugeniag@users.noreply.github.com>
2023-02-24 11:20:53 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,120 @@
+// Intro a JavaScript
+
+// {
+//     const miNombre = "Eugenia";
+// console.log(miNombre);
+// const miApellido = "Gonzalez";
+// console.log(miApellido);
+// let miEdad = 27;
+// console.log(miEdad);
+// let miMascota = "Palta";
+// console.log(miMascota)
+// let edadMascota = 2;
+// console.log(edadMascota);
+// let nombreCompleto = miNombre +" "+ miApellido;
+// console.log(nombreCompleto);
+// let textoPresentacion = {
+// }
+// let sumaEdades = miEdad + edadMascota;
+// let restaEdades = miEdad - edadMascota;
+// let productoEdades = miEdad*edadMascota;
+// let divisionEdades = miEdad/edadMascota;
+// }
+// // Utilizando prompt
+
+// let miNombre = prompt("Ingrese su nombre: ");
+// console.log(miNombre);
+// let miApellido = prompt("Ingrese su apellido: ");
+// console.log(miApellido);
+// let miEdad = Number(prompt("Ingrese su edad: "));
+// console.log(miEdad);
+// let miMascota = prompt("Ingrese el nombre de su mascota: ");
+// console.log(miMascota);
+// let edadMascota = Number (prompt("Ingrese la edad de su mascota: "));
+// console.log(edadMascota);
+// let sumaEdades = miEdad + edadMascota;
+// console.log(sumaEdades);
+// let restaEdades = miEdad - edadMascota;
+// console.log(restaEdades);
+// let productoEdades = miEdad*edadMascota;
+// console.log(productoEdades);
+// let divisionEdades = miEdad/edadMascota;
+// console.log(divisionEdades);
+// let vivoEn = "Resistencia";
+// let miNacionalidad = "Argentina";
+// let color= "negro";
+// let esMalo= false;
+// let suTamanio= "pequeño";
+
+
+// //Objeto
+// let alumno = {
+// miNombre,
+// miApellido,
+// miEdad,
+// vivoEn,
+// miNacionalidad,
+// }
+// console.table(alumno);
+// console.table(miNombre);
+// console.table(miApellido);
+// console.table(miEdad);
+// console.table(vivoEn);
+// console.table(miNacionalidad);
+
+// let mascota = {
+//     miMascota,
+//     edadMascota,
+//     color,
+//     esMalo,
+//     suTamanio,
+// }
+// console.table(mascota);
+// console.table(miMascota);
+// console.table(edadMascota);
+// console.table(color);
+// console.table(esMalo);
+// console.table(suTamanio);
+
+// Array
+let frutas = ["Banana", "Naranja", "Manzana", "Kiwi", "Frutillas"];
+console.log(frutas);
+console.log(frutas[0]);
+console.log(frutas[1]);
+console.log(frutas[2]);
+console.log(frutas[3]);
+console.log(frutas[4]);
+
+let numeros = [6, 23, 10, 6, 2, 10, 3, 4];
+console.log(numeros);
+console.log(numeros[0]);
+console.log(numeros[1]);
+console.log(numeros[2]);
+console.log(numeros[3]);
+console.log(numeros[4]);
+console.log(numeros[5]);
+console.log(numeros[6]);
+console.log(numeros[7]);
+
+let familia = ["Diego", "Eugenia", "Amelia", "Almendra", "Palta"];
+console.log(familia);
+console.log(familia[0]);
+console.log(familia[1]);
+console.log(familia[2]);
+console.log(familia[3]);
+console.log(familia[4]);
+
+//Texto Aleatorio usando: 
+
+frutas[1], numeros[3], familia[4]
+console.log("Verduleria la "+ frutas[1]+ " " + "abierta "+ numeros[3]+" "+ "dias de la semana. Tenemos excelente " + familia[4] )
+
+
+{
+let miEdad = prompt("Ingrese su edad: ");
+let otraEdad= prompt("Ingrese la edad de un compañero: ");
+console.log(miEdad);
+console.log(otraEdad);
+let edadesIguales = (miEdad=== otraEdad)
+console.log(edadesIguales);
+}
